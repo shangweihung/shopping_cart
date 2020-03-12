436e0b57cd07bdd476d7bf0fc142445d02af3135
shangweihung <alannn1230@gmail.com>
2020-03-11 19:54:18 -0700

    get all product

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const product = require('./routes/product');
+
 var app = express();
 
-const product = require('./routes/product');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/', product);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,9 +42,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-
-
-app.use('/', product);
 
 module.exports = app;
--- controllers/product/get_controller.js
+++ controllers/product/get_controller.js
@@ -1,16 +1,16 @@
 const productData = require('../../models/product/getAllProduct_model');
 
-module.exports = class GetProduct{
-    // get all product information
-    getAllProduct(req, res, next){
-        productData().then(result => {
-            res.json({
-                result: result
-            })
-        }, (err) => {
-            res.json({
-                result: err
-            })
+module.exports = class GetProduct {
+    // 取得全部產品資料
+    getAllProduct(req, res, next) {
+      productData().then(result => {
+        res.json({
+            result: result
         })
-    }
+      }, (err) => {
+        res.json({
+            result: err
+        })
+      })
+    } 
 }
